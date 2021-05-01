d3331379efddbe574f782dc9ba41d68231e889d6
Lou Markovski <lou.markovski@gmail.com>
2021-05-02 08:59:19 +1000

    task/Ancillary-function-check_defined
    makefiles/makester.mk
    * New ancillary function "check_defined" that checks that given variable
      is set and has non-empty value.  Exits with an error otherwise.
      See https://stackoverflow.com/questions/10858261/abort-makefile-if-variable-not-set

--- makefiles/makester.mk
+++ makefiles/makester.mk
@@ -28,6 +28,20 @@ clean:
 submodule-update:
 	$(GIT) submodule update --remote --merge
 
+# Check that given variables are set and all have non-empty values.
+# Exit with an error otherwise.
+# See https://stackoverflow.com/questions/10858261/abort-makefile-if-variable-not-set
+#
+# Params:
+#   1. Variable name(s) to test.
+#   2. (optional) Error message to print.
+check_defined = \
+    $(strip $(foreach 1,$1, \
+        $(call __check_defined,$1,$(strip $(value 2)))))
+__check_defined = \
+    $(if $(value $1),, \
+        $(error Undefined $1$(if $2, ($2))))
+
 vars:
 	@echo "\n\
   HASH                               $(HASH)\n\
