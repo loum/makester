ee0ad67dc0b403503c10726c62e5ae03739f91d3
Lou Markovski <lou.markovski@gmail.com>
2021-09-29 08:18:18 +1000

    task/Extend-docker-image-tagging-to-include-main-versions

--- makefiles/docker.mk
+++ makefiles/docker.mk
@@ -55,7 +55,7 @@ endif
 
 IMAGE_TAG_ID = $(shell $(DOCKER) images --filter=reference=$(MAKESTER__SERVICE_NAME) --format "{{.ID}}" | head -1)
 MAKESTER__IMAGE_TAG_ALIAS = $(MAKESTER__SERVICE_NAME):$(MAKESTER__IMAGE_TARGET_TAG)
-tag:
+tag tag-image:
 	-$(DOCKER) tag $(IMAGE_TAG_ID) $(MAKESTER__IMAGE_TAG_ALIAS)
 
 tag-latest: MAKESTER__IMAGE_TARGET_TAG = latest
@@ -70,8 +70,14 @@ tag-version: tag
 tag-rm-version: MAKESTER__IMAGE_TARGET_TAG = $(MAKESTER__VERSION)-$(MAKESTER__RELEASE_NUMBER)
 tag-rm-version: rmi
 
+tag-main: MAKESTER__IMAGE_TARGET_TAG = $(MAKESTER__VERSION)
+tag-main: tag
+
+tag-rm-main: MAKESTER__IMAGE_TARGET_TAG = $(MAKESTER__VERSION)
+tag-rm-main: rmi
+
 image-push:
-	$(DOCKER) push $(MAKESTER__IMAGE_TAG_ALIAS)
+	-$(DOCKER) push $(MAKESTER__IMAGE_TAG_ALIAS)
 
 rm-dangling-images:
 	$(shell $(DOCKER) rmi $($(DOCKER) images -q -f dangling=true`))
@@ -88,8 +94,14 @@ docker-help:
   bash                 Bash on container $(MAKESTER__CONTAINER_NAME) as \"USER\"\n\
   logs                 Follow container $(MAKESTER__CONTAINER_NAME) logs (Ctrl-C to end)\n\
   stop                 Stop container $(MAKESTER__CONTAINER_NAME)\n\
+  tag-image            Tag image $(MAKESTER__SERVICE_NAME) \"$(HASH)\"\n\
+  tag-rm-image         Undo \"tag-image\"\n\
   tag-latest           Tag image $(MAKESTER__SERVICE_NAME) \"latest\"\n\
+  tag-rm-latest        Undo \"tag-latest\"\n\
   tag-version          Tag image $(MAKESTER__SERVICE_NAME) \"$(MAKESTER__VERSION)-$(MAKESTER__RELEASE_NUMBER)\"\n\
+  tag-rm-version       Undo \"tag-version\"\n\
+  tag-main             Tag image $(MAKESTER__SERVICE_NAME) \"$(MAKESTER__VERSION)\"\n\
+  tag-rm-main          Undo \"tag-main\"\n\
   rm-dangling-images   Remove all dangling images\n\
   image-push           Push image \"$(MAKESTER__IMAGE_TAG_ALIAS)\"\n"
 
