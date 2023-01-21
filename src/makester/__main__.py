85827234623fc71cefb15c57375fbc4d3f6ce47d
Lou Markovski <lou.markovski@gmail.com>
2023-01-22 09:22:56 +1100

    task/Typer-for-cli
    setup.cfg
    * add PyPI typer[all] as a dependency
    
    src/makester/cli.py => src/makester/__main__.py
    * traditional CLI module factored into __main__.py

--- src/makester/__main__.py
+++ src/makester/__main__.py
@@ -1,7 +1,100 @@
 """Makester CLI.
 
 """
-from makester.cli import main
+from dataclasses import dataclass
+from typing import Text
+import json
+
+from logga import log, suppress_logging
+import typer
+
+import makester.templater
+import makester.waitster
+
+app = typer.Typer(
+    add_completion=False,
+    help="Makester CLI tool",
+)
+
+
+@dataclass
+class Common:
+    """Common arguments at the command level."""
+
+    quiet: bool
+
+
+@app.command()
+def templater(
+    template: Text = typer.Argument(
+        ...,
+        help="Path to Jinja2 template (absolute, or relative to user home)",
+        show_default=False,
+    ),
+    env_filter: Text = typer.Option(
+        None,
+        "--filter",
+        "-f",
+        help="Environment variable filter (ignored when mapping is taken from JSON file)",
+        show_default=False,
+    ),
+    mapping: Text = typer.Option(
+        None,
+        "--mapping",
+        "-m",
+        help="path to JSON mappings (absolute, or relative to user home)",
+        show_default=False,
+    ),
+    write: bool = typer.Option(
+        False,
+        "--write",
+        "-w",
+        help="Write out templated file alongside Jinja2 template",
+    ),
+):
+    """Template against environment variables or optional JSON values."""
+    mappings = {}
+    if mapping:
+        mappings.update(makester.templater.get_json_values(mapping))
+    else:
+        mappings.update(makester.templater.get_environment_values(token=(env_filter)))
+
+    log.info("Template mapping values sourced:\n%s", json.dumps(mappings, indent=2))
+
+    makester.templater.build_from_template(mappings, template, write_output=write)
+
+
+@app.command()
+def backoff(
+    host: Text = typer.Argument(
+        ..., help="Host name of service connection.", show_default=False
+    ),
+    port: int = typer.Argument(..., help="Service port number.", show_default=False),
+    detail: Text = typer.Option(
+        "Service", "--detail", "-d", help="Meaningful description for backoff port"
+    ),
+):
+    """Wait until dependent service is ready."""
+    makester.waitster.port_backoff(host, port, detail)
+
+
+@app.callback()
+def common(
+    ctx: typer.Context,
+    quiet: bool = typer.Option(
+        False, "--quiet", help='Disable logs to screen (to log level "ERROR")'
+    ),
+):
+    """Define the common arguments."""
+    ctx.obj = Common(quiet)
+
+    if ctx.obj.quiet:
+        suppress_logging()
+
+
+def main():
+    """Script entry point."""
+    app()
 
 
 if __name__ == "__main__":
---
+++ src/makester/waitster.py
@@ -0,0 +1,28 @@
+"""Wait until dependent service is ready.
+
+"""
+from typing import Text
+import telnetlib
+
+from logga import log
+import backoff
+
+
+@backoff.on_exception(
+    backoff.constant,
+    (OSError, EOFError, ConnectionRefusedError),
+    max_time=300,
+    interval=5,
+)
+def port_backoff(host: Text, port: int, detail: Text):
+    """Service backoff until ready."""
+    msg = f"Checking host:port {host}:{port}"
+    if detail is not None:
+        msg += f" {detail}"
+
+    log.info("%s ...", msg)
+
+    with telnetlib.Telnet(host, int(port)) as _tn:
+        _tn.set_debuglevel(5)
+        _tn.read_until(b" ", 1)
+        log.info("Port %s ready", port)
